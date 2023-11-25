887bdb5ae9fa8a6fe01b5c83d607176163582283
Ivan Mahút <ivan.mahut.92@gmail.com>
2023-11-25 17:09:19 +0100

    DB update

--- Database/create_db.sql
+++ Database/create_db.sql
@@ -91,13 +91,21 @@ CREATE TABLE PREFERED_SLOTS_ACTIVITY (
     preference ENUM('Prefers', 'Disprefers')NOT NULL
 );
 
+CREATE TABLE SUBJECT_TEACHERS(
+    /*PK*/
+    sub_teach_ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    /*FK*/
+    user_ID INT REFERENCES USERS(user_ID),
+    subject_ID INT REFERENCES SUBJECTS(subject_ID)
+);
+
 -- Inserting mock data into USERS
 INSERT INTO USERS(username, hashed_password, email, user_role) VALUES
 ('Admin', '$2y$10$9mSWZYKjJW9YW9tgdYZa9uUuNVr9zhzcT0iOzruQo9w5KGqizrAv2', 'user1@email.com', 'Admin'),     /*password1*/
 ('Guarantor', '$2y$10$KJYI.m9s/DRAtarK3SVD3efnyNygYdyjKFf1XoFNZdQEphb1/lLtG', 'user2@email.com', 'Guarantor'), /*password2*/
 ('Teacher', '$2y$10$iPVH5GDLM3YYMD5v53xrWu9qoNEyV11SzAzi4sWhJzAxQN/ZgPnqu', 'user3@email.com', 'Teacher'),   /*password3*/
 ('Scheduler', '$2y$10$m3D3CAvaD9AjCla2qtnrNu4bHwFxn93ufVqBcFlTAUANzwMwVbeYG', 'user4@email.com', 'Scheduler'),  /*password4*/
-('Student', '$2y$10$BjA2J9QWBIqO49t5JEi3n.0ihgNljwGN4ZrJyhFzdr/KkMVPBaie2', 'user5@email.com', 'Student'),  /*password5*/
+('Student', '$2y$10$BjA2J9QWBIqO49t5JEi3n.0ihgNljwGN4ZrJyhFzdr/KkMVPBaie2', 'user5@email.com', 'Student');  /*password5*/
 
 -- Inserting mock data into ROOM
 INSERT INTO ROOM (room_name, capacity, room_location) VALUES
